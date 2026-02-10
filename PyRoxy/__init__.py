286b41e122d726eec62536ce42a65ec79682b0c4
tov6055-del <tov6055@gmail.com>
2026-02-10 16:32:05 +0300

    Update __init__.py

--- PyRoxy/__init__.py
+++ PyRoxy/__init__.py
@@ -125,7 +125,7 @@ class Proxy(object):
         with suppress(Exception):
             with self.open_socket() as sock:
                 sock.settimeout(timeout)
-                sock.connect(("gw.sandboxol.com", 80))
+                sock.connect(("gwbyte.sandboxol.com", 80))
                 sock.sendall(b"GET /server-time HTTP/1.1\r\nHost: gw.sandboxol.com\r\n\r\n")
                 if b'{"code":1,"' in sock.recv(4096):
                     print("True")
